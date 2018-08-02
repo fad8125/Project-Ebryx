4dd177f15001f048915e05215045a8e0d920765a
Muhammad Fahd Asif <2262555a@student.gla.ac.uk>
2018-08-03 00:36:01 +0100

    GoogleMapit- shows google map
    - displaying the map using google API key and taking assistance from angular docs
    -used bootstrap for designing the navigation bar
    -worked on the app.component html mainly for displaying the navigation bar

--- google-map/src/app/app.component.ts
+++ google-map/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'My first ';
+  lat: number = 31.5204;
+  lng: number = 74.3587;
 }
